3b4e2c3b2ada1bf1e15363e604c2b1b6485beb39
Jacky Wang <jackycwwang@gmail.com>
2022-03-03 22:47:31 -0500

    Complete pipeline and base model

--- help_functions.py
+++ help_functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.impute import KNNImputer
 
@@ -13,8 +15,7 @@ cat_col_without_order = ['Type_of_collision',
                          'Sex_of_casualty',
                          'Road_surface_conditions',
                          'Sex_of_driver',
-                         'Vehicle_movement',
-                         'Work_of_casuality',
+                         'Vehicle_movement',                         
                          'Types_of_Junction',
                          'Type_of_vehicle',
                          'Vehicle_driver_relation',
@@ -28,6 +29,7 @@ cat_col_without_order = ['Type_of_collision',
                          'Lanes_or_Medians',
                          'Area_accident_occured']
 
+
 map_dicts = {
     'Day_of_week': {'Monday': 1, "Tuesday": 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 0},
     'Age_band_of_driver': {'Under 18': 0, '18-30': 1, '31-50': 2, 'Over 51': 3},
@@ -38,37 +40,19 @@ map_dicts = {
     'Defect_of_vehicle': {'No defect': 0, '5': 5, '7': 7}
 }
 
-
-
-def preprocessing(df):
+def preprocess(df):
     df = df.replace('Unknown', np.nan)
     df = df.replace('na', np.nan)
-    df = df.drop(['Work_of_casuality'], axis=1)    
+    df = df.drop(['Work_of_casuality'], axis=1)
     df.Time = pd.to_datetime(df.Time).dt.hour
     
     for col in cat_col_with_order:
         df[col] = df[col].map(map_dicts[col])
+    return df
     
-    cat_cols = df.select_dtypes(include='object').columns    
-    num_cols = df.select_dtypes(exclude='object').columns    
-    num_vals = df.select_dtypes(exclude='object').to_numpy()
+def label_encoder(df):    
+    mask = df.isnull()
+    labels = df.apply(LabelEncoder().fit_transform)
+    df_label_encoded = labels.where(~mask, df)
     
-    label_lst = []
-    for col in cat_cols:
-        le = LabelEncoder()
-        nan_label = df[col].nunique()        
-        labels = le.fit_transform(df[col])
-        labels = np.where(labels==nan_label, np.nan, labels)
-        label_lst.append(labels)
-    cat_labels = np.array(label_lst).transpose()
-    label_encoded = np.concatenate((cat_labels, num_vals), axis=1)
-    
-    knn_imputer = KNNImputer(n_neighbors=5, 
-                             weights="uniform", 
-                             metric='nan_euclidean')
-    imputed_data = knn_imputer.fit_transform(label_encoded)
-    df_clean = pd.DataFrame(imputed_data)
-    
-    df_clean.columns = np.concatenate((cat_cols, num_cols))  
-    
-    return df_clean
+    return df_label_encoded
